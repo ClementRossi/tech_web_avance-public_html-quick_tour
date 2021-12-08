cd1a699de28c1f3e8f98cbb8aa92b9e9eacd1472
ClementRossi <clement.rossi98@yahoo.com>
2021-12-08 21:21:50 +0100

    try

--- public_html/quick_tour/src/Controller/Admin/DashboardController.php
+++ public_html/quick_tour/src/Controller/Admin/DashboardController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
-use App\Entity\Tournoi;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,6 +27,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Tournois', 'fas fa-list', Tournoi::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
